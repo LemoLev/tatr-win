ae23782d9067151eb417f457f1b72f6f1a01f00b
rexim <reximkut@gmail.com>
2026-08-25 22:04:02 +0700

    Always create TAGS property even when no tags were provided

--- src/tatr.c
+++ src/tatr.c
@@ -194,14 +194,12 @@ static void append_task_md_content(String_Builder *sb, Task task)
     sb_appendf(sb, "\n");
     sb_appendf(sb, "- STATUS: "SV_Fmt"\n", SV_Arg(task.status));
     sb_appendf(sb, "- PRIORITY: %d\n", task.priority);
-    if (task.tags.count > 0) {
-        sb_appendf(sb, "- TAGS: ");
-        for (size_t i = 0; i < task.tags.count; ++i) {
-            if (i > 0) sb_appendf(sb, ",");
-            sb_append_buf(sb, task.tags.items[i].data, task.tags.items[i].count);
-        }
-        sb_appendf(sb, "\n");
+    sb_appendf(sb, "- TAGS: ");
+    for (size_t i = 0; i < task.tags.count; ++i) {
+        if (i > 0) sb_appendf(sb, ",");
+        sb_append_buf(sb, task.tags.items[i].data, task.tags.items[i].count);
     }
+    sb_appendf(sb, "\n");
     sb_appendf(sb, "\n");
     sb_appendf(sb, "No description.\n");
 }
